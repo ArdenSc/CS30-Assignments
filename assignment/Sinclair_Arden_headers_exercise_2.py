64239db52adfb1d535cd233b4dce352bcf06a47f
Arden Sinclair <arden.sinclair@rbe.sk.ca>
2020-09-14 08:57:24 -0600

    Added Headers in Python assignment

---
+++ assignment/Sinclair_Arden_headers_exercise_2.py
@@ -0,0 +1,10 @@
+# Arden Sinclair
+# CS30 P1Q1
+# Sept. 14, 2020
+# Prints the name Arden with all of the
+# Python capitalization methods
+
+name = "Arden"
+print(name.lower())
+print(name.upper())
+print(name.title())
