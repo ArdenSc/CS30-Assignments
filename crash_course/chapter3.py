3fc74cd4a99c1113def1b8c996d801634bdb5522
Arden Sinclair <arden.sinclair@rbe.sk.ca>
2020-09-15 10:18:24 -0600

    Added chapter 3 7

--- crash_course/chapter3.py
+++ crash_course/chapter3.py
@@ -53,5 +53,28 @@ def exercise6():
         print(message.format(guest))
 
 
+def exercise7():
+    guests = ["Jeff Bezos", "Bill Gates", "Elon Musk"]
+    message = "Dear {}, I would like to invite you to dinner."
+    for guest in guests:
+        print(message.format(guest))
+    print("We have a new location so more guests can attend.")
+    guests.insert(0, "Steve Jobs")
+    guests.insert(2, "Larry Page")
+    guests.append("Sergey Brin")
+    for guest in guests:
+        print(message.format(guest))
+    removal = "Sorry {}, you can't be invted to dinner."
+    print(removal.format(guests.pop(0)))
+    print(removal.format(guests.pop(2)))
+    print(removal.format(guests.pop(1)))
+    print(removal.format(guests.pop(2)))
+    for guest in guests:
+        print(message.format(guest))
+    del guests[1]
+    del guests[0]
+    print(guests)
+
+
 if __name__ == "__main__":
     runall(import_module(__name__), "exercise")
