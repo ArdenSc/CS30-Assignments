6d41316f2402f9bd93933e43f24cc5ce7195770c
Arden Sinclair <arden.sinclair@rbe.sk.ca>
2020-09-18 10:58:34 -0600

    Added rpg numerical list

---
+++ assignment/Sinclair_Arden_rpg_numerical_list.py
@@ -0,0 +1,31 @@
+# Arden Sinclair
+# CS30 P1Q1
+# Sept. 18, 2020
+# An example program that takes normal lists and
+# prints them as formatted numerical lists.
+
+from typing import List
+
+
+def num_list(name: str, list: List[str]):
+    """Prints numerical list with one item per line.
+
+    Args:
+        name: The name of the list.
+        list: The list of values.
+    """
+    print(f"{name}:")
+    for index, value in enumerate(list):
+        print(f"{index + 1}: {value.capitalize()}")
+
+# Define the lists.
+player_weapons = ["sword", "dagger", "club"]
+player_armour = ["helmet", "chestplate", "leggings", "boots"]
+
+# Print the lists unformatted.
+print(player_weapons)
+print(player_armour)
+
+# Print the lists as a formatted numerical list.
+num_list('Player Weapons', player_weapons)
+num_list("Player Armour", player_armour)
