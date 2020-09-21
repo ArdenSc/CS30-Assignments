4d6e374db76658ce7696529619bbbb2e4d80d0e5
Arden Sinclair <arden.sinclair@rbe.sk.ca>
2020-09-21 08:50:18 -0600

    Added chapter 4 10-11

--- crash_course/chapter4.py
+++ crash_course/chapter4.py
@@ -41,5 +41,34 @@ def exercise9():
     print([n**3 for n in range(1, 11)])
 
 
+def exercise10():
+    numbers = [n**3 for n in range(1, 11)]
+    print("The first three itmes in the list are:")
+    print(numbers[:2])
+    print("Three items from the middle of the list are:")
+    print(numbers[2:5])
+    print("The last three itmes in the list are:")
+    print(numbers[-3:])
+
+
+def exercise11():
+    pizzas = ["pepperoni", "all dressed", "meat lovers"]
+    for pizza in pizzas:
+        print(f"I like {pizza} pizza.")
+    print(f"My favorite pizza is {pizzas[0]}")
+    print(f"My second favorite is {pizzas[1]}")
+    print(f"My third favorite pizza is {pizzas[2]}")
+    print("I really love pizza!")
+    friend_pizzas = pizzas[:]
+    pizzas.append("ham and pineapple")
+    friend_pizzas.append("cheese")
+    print("My favorite pizzas are:")
+    for pizza in pizzas:
+        print(pizza)
+    print("My friend's favorite pizzs are:")
+    for pizza in friend_pizzas:
+        print(pizza)
+
+
 if __name__ == "__main__":
     runall(import_module(__name__), "exercise")
