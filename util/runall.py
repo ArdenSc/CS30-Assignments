716b70a8d7111df7d6c9b2bdc2c582651f2c0dc8
Arden Sinclair <arden.sinclair@rbe.sk.ca>
2020-09-24 09:36:58 -0600

    Fixed the running order of runall Added a value for sorting the items

--- util/runall.py
+++ util/runall.py
@@ -1,11 +1,14 @@
 from types import ModuleType
+from typing import Callable
 
 
-def runall(module: ModuleType, startswith: str):
-    for i in dir(module):
-        if not i.startswith(startswith): continue
+def runall(module: ModuleType,
+           startswith: str,
+           key: Callable[[str], int] = lambda x: int(x[8:])):
+    items = [i for i in dir(module) if i.startswith(startswith)]
+    for i in sorted(items, key=key):
         item = getattr(module, i)
         if not callable(item): continue
         print(f"{i}\n")
         item()
-        print("")
+        print()
