2e64bb7910d848b7e8cc045f14e69367c41cf1bb
Arden Sinclair <general@ardensinclair.com>
2020-10-01 22:01:19 -0600

    More detailed setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name="Assignments", version="0.10", packages=find_packages())
+setup(name="Assignments",
+      version="0.1.1",
+      packages=find_packages(exclude=["tests"]),
+      author="Arden Sinclair",
+      description="Assignments for CS30",
+      url="https://github.com/ArdenSinclair/CS30-Assignments",
+      tests_require=["pytest"])
